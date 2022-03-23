408ff7fda6c6aaba0b8ebb40e7a3109dbba5d306
alisson <alissoneatena1@gmail.com>
2022-03-23 11:14:01 -0300

    configuration scss to screen shell

--- pet-shop/apps/admin/src/app/app.module.ts
+++ pet-shop/apps/admin/src/app/app.module.ts
@@ -1,13 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import {  RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { ShellComponent } from "./shared/shell/shell.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
 
+const routes : Routes = [
+    {path:"", component:ShellComponent, children:
+    [
+        {
+            path:"dashboard", component:DashboardComponent
+        }
+    ]}
+];
 
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule],
+    declarations: [AppComponent, ShellComponent, SidebarComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes, {initialNavigation:"enabled"})],
     providers: [],
     bootstrap: [AppComponent],
 })
