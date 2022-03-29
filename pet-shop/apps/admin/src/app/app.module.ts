84f10e7abc963152ff8c10bff2317396b2d8fb99
Alisson Davis <alissoneatena1@gmail.com>
2022-03-28 21:04:32 -0300

    changes on admin shell

--- pet-shop/apps/admin/src/app/app.module.ts
+++ pet-shop/apps/admin/src/app/app.module.ts
@@ -6,20 +6,37 @@ import { AppComponent } from "./app.component";
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { ShellComponent } from "./shared/shell/shell.component";
 import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+
+import {CardModule} from 'primeng/card';
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
 
 const routes : Routes = [
     {path:"", component:ShellComponent, children:
     [
         {
             path:"dashboard", component:DashboardComponent
+        },
+        {
+            path:"categorias", component:CategoriesListComponent
         }
     ]}
 ];
 
 
 @NgModule({
-    declarations: [AppComponent, ShellComponent, SidebarComponent],
-    imports: [BrowserModule, RouterModule.forRoot(routes, {initialNavigation:"enabled"})],
+    declarations: [AppComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
+    imports: [
+        BrowserModule, 
+        RouterModule.forRoot(routes, {initialNavigation:"enabled"}),
+        CardModule,
+        ToolbarModule,
+        ButtonModule,
+        TableModule
+    ],
+    
     providers: [],
     bootstrap: [AppComponent],
 })
